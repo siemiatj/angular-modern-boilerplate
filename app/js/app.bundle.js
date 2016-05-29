2389d0b599151dc723ec691f88cf47ff93e00e80
siemiatj <kuba@saucelabs.com>
2016-05-29 01:58:17 -0700

    - install missing packages - cleanup bundle

--- app/js/app.bundle.js
+++ app/js/app.bundle.js
@@ -1,21 +1,11 @@
-'use strict';
-
 /* App Bundle */
-var angularAnimate = require('../../node_modules/angular-animate');
-var angularMaterial = require('../../node_modules/angular-material');
-
-require('angular-material/angular-material.css');
-require('angular-material/layouts/angular-material.layout-attributes.scss');
-require('font-awesome/css/font-awesome.css');
-
-require('../../node_modules/angular-material/');
+require('../../node_modules/angular-material');
 require('./services');
 require('./controllers.es6');
 require('./directives.es6');
 
 angular.module('app', [
-  angularAnimate,
-  angularMaterial,
+  'ngMaterial',
   'app.services',
   'app.controllers',
   'app.directives'
